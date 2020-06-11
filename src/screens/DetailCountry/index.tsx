67a61374b82802bd566c85f6bcd5447e519b8634
arturdeveloper1 <arturdeveloper1@gmail.com>
2020-06-11 00:17:34 -0300

    fix styles

--- src/screens/DetailCountry/index.tsx
+++ src/screens/DetailCountry/index.tsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
-import {useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import api from '../../services/api';
 import { Feather as Icon } from '@expo/vector-icons';
 
 export default function Detail() {
-
-
-
     const navigation = useNavigation();
 
-  function navigationToDashboard(){
-    navigation.navigate('Dashboard');
-}
+    function navigationToDashboard() {
+        navigation.navigate('Dashboard');
+    };
+
     return (
         <TouchableOpacity onPress={navigationToDashboard}>
-        <View>
-        <Icon name="arrow-right" size={40} color="black" />
-        </View>
+            <View>
+                <Icon name="arrow-right" size={40} color="black" />
+            </View>
         </TouchableOpacity>
     );
-}
+};
